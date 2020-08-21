0b388c80e6ca389ab65b3b689ebf65f43b06e9a5
César Magalhães <ceanma@gmail.com>
2020-08-21 14:54:41 -0300

    Foi incluida a funcionalidade para conexão WiFi

--- main/app_connect.c
+++ main/app_connect.c
@@ -5,19 +5,176 @@
  *      Author: ceanm
  */
 
-#include "app_connect.h"
-
+#include <string.h>
 #include "esp_log.h"
 #include "esp_err.h"
+#include "esp_wifi.h"
+#include "esp_wifi_default.h"
+#include "esp_netif.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "sdkconfig.h"
+
+#include "app_connect.h"
+
+#define GOT_IPV4_BIT (1ULL << 0)
+#define GOT_IPV6_BIT (1ULL << 1)
+
+#ifdef CONFIG_APP_CONNECT_IPV6
+#define CONNECTED_BITS (GOT_IPV4_BIT | GOT_IPV6_BIT)
+#else
+#define CONNECTED_BITS (GOT_IPV4_BIT)
+#endif
 
 static const char *TAG = "app_connect";
 
+static EventGroupHandle_t s_app_connect_event_group;
+
+static esp_netif_t *s_app_esp_netif = NULL;
+static esp_ip4_addr_t s_ip_addr;
+
+#ifdef CONFIG_APP_CONNECT_IPV6
+static esp_ip6_addr_t s_ipv6_addr;
+#endif
+
+static const char *s_connection_name;
+
+/* set up connection, Wi-Fi or Ethernet */
+static void start(void);
+/* tear down connection, release resources */
+static void stop(void);
+
+static void on_wifi_disconnect(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data);
+static void on_got_ip(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data);
+
+#ifdef CONFIG_APP_CONNECT_IPV6
+static void on_wifi_connect(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data);
+static void on_got_ipv6(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data);
+#endif
+
 esp_err_t app_connect(void) {
-	ESP_LOGI(TAG, "A funcionalidade app_connect foi executada");
+	if (s_app_connect_event_group != NULL)
+		return ESP_ERR_INVALID_STATE;
+
+	s_app_connect_event_group = xEventGroupCreate();
+
+	start();
+
+	ESP_ERROR_CHECK(esp_register_shutdown_handler(&stop));
+
+	ESP_LOGI(TAG, "Waiting for IP");
+	xEventGroupWaitBits(s_app_connect_event_group, CONNECTED_BITS, true, true, portMAX_DELAY);
+
+	ESP_LOGI(TAG, "Connected to %s", s_connection_name);
+	ESP_LOGI(TAG, "IPv4 address: " IPSTR, IP2STR(&s_ip_addr));
+#ifdef CONFIG_APP_CONNECT_IPV6
+    ESP_LOGI(TAG, "IPv6 address: " IPV6STR, IPV62STR(s_ipv6_addr));
+#endif
+
 	return ESP_OK;
 }
 
 esp_err_t app_disconnect(void) {
-	ESP_LOGI(TAG, "A funcionalidade app_disconnect foi executada");
+	if (s_app_connect_event_group == NULL)
+		return ESP_ERR_INVALID_STATE;
+
+	vEventGroupDelete(s_app_connect_event_group);
+	s_app_connect_event_group = NULL;
+
+	stop();
+
+	ESP_LOGI(TAG, "Disconnected from %s", s_connection_name);
+	s_connection_name = NULL;
+
 	return ESP_OK;
 }
+
+static void start(void) {
+	wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+	ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+	esp_netif_config_t netif_config = ESP_NETIF_DEFAULT_WIFI_STA();
+	esp_netif_t *netif = esp_netif_new(&netif_config);
+	assert(netif);
+	esp_netif_attach_wifi_station(netif);
+	esp_wifi_set_default_wifi_sta_handlers();
+	s_app_esp_netif = netif;
+
+	ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, &on_wifi_disconnect, NULL));
+	ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &on_got_ip, NULL));
+#ifdef CONFIG_APP_CONNECT_IPV6
+	ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, WIFI_EVENT_STA_CONNECTED, &on_wifi_connect, netif));
+	ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_GOT_IP6, &on_got_ipv6, NULL));
+#endif
+
+	ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+	wifi_config_t wifi_config = {
+		.sta = {
+			.ssid = CONFIG_APP_WIFI_SSID,
+			.password = CONFIG_APP_WIFI_PASSWORD,
+		},
+	};
+	ESP_LOGI(TAG, "Connecting to %s...", wifi_config.sta.ssid);
+	ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+	ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
+	ESP_ERROR_CHECK(esp_wifi_start());
+	ESP_ERROR_CHECK(esp_wifi_connect());
+	s_connection_name = CONFIG_APP_WIFI_SSID;
+}
+
+static void stop(void) {
+	ESP_ERROR_CHECK(esp_event_handler_unregister(WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, &on_wifi_disconnect));
+	ESP_ERROR_CHECK(esp_event_handler_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, &on_got_ip));
+#ifdef CONFIG_APP_CONNECT_IPV6
+    ESP_ERROR_CHECK(esp_event_handler_unregister(IP_EVENT, IP_EVENT_GOT_IP6, &on_got_ipv6));
+    ESP_ERROR_CHECK(esp_event_handler_unregister(WIFI_EVENT, WIFI_EVENT_STA_CONNECTED, &on_wifi_connect));
+#endif
+    esp_err_t err = esp_wifi_stop();
+	if (err == ESP_ERR_WIFI_NOT_INIT)
+		return;
+
+	ESP_ERROR_CHECK(err);
+	ESP_ERROR_CHECK(esp_wifi_deinit());
+	ESP_ERROR_CHECK(esp_wifi_clear_default_wifi_driver_and_handlers(s_app_esp_netif));
+	esp_netif_destroy(s_app_esp_netif);
+	s_app_esp_netif = NULL;
+}
+
+static void on_wifi_disconnect(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data) {
+	xEventGroupClearBits(s_app_connect_event_group, GOT_IPV4_BIT);
+#ifdef CONFIG_APP_CONNECT_IPV6
+	xEventGroupClearBits(s_app_connect_event_group, GOT_IPV6_BIT);
+#endif
+
+	ESP_LOGI(TAG, "Wi-Fi disconnected, trying to reconnect...");
+	esp_err_t err = esp_wifi_connect();
+	if (err == ESP_ERR_WIFI_NOT_STARTED)
+		return;
+
+	ESP_ERROR_CHECK(err);
+}
+
+static void on_got_ip(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data) {
+	ESP_LOGI(TAG, "Got IP event!");
+	ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
+	memcpy(&s_ip_addr, &event->ip_info.ip, sizeof(s_ip_addr));
+	xEventGroupSetBits(s_app_connect_event_group, GOT_IPV4_BIT);
+}
+
+#ifdef CONFIG_APP_CONNECT_IPV6
+static void on_wifi_connect(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data) {
+	esp_netif_create_ip6_linklocal(arg);
+}
+
+static void on_got_ipv6(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data) {
+	ip_event_got_ip6_t *event = (ip_event_got_ip6_t *)event_data;
+	if (event->esp_netif != s_app_esp_netif) {
+		ESP_LOGD(TAG, "Got IPv6 from another netif: ignored");
+		return;
+	}
+	ESP_LOGI(TAG, "Got IPv6 event!");
+	memcpy(&s_ipv6_addr, &event->ip6_info.ip, sizeof(s_ipv6_addr));
+	xEventGroupSetBits(s_app_connect_event_group, GOT_IPV6_BIT);
+}
+#endif
