a6057e708101516acd568fd0b0a1aa9c198b3f71
César Magalhães <ceanma@gmail.com>
2020-08-23 16:14:42 -0300

    Foram criadas a funções JSON_GET_INT, JSON_GET_DOUBLE e JSON_GET_STRING

--- main/include/app_common.h
+++ main/include/app_common.h
@@ -12,18 +12,23 @@ extern "C" {
 #endif
 
 #include "esp_log.h"
+#include "cJSON.h"
 
 #define APP_COMMON_TAG "app_common"
 
-#define APP_ERROR_CHECK(a, str, goto_tag, ...)                                             \
-    do                                                                                     \
-    {                                                                                      \
-        if (!(a))                                                                          \
-        {                                                                                  \
+#define APP_ERROR_CHECK(a, str, goto_tag, ...)                                               \
+    do                                                                                       \
+    {                                                                                        \
+        if (!(a))                                                                            \
+        {                                                                                    \
             ESP_LOGE(APP_COMMON_TAG, "%s(%d): " str, __FUNCTION__, __LINE__, ##__VA_ARGS__); \
-            goto goto_tag;                                                                 \
-        }                                                                                  \
+            goto goto_tag;                                                                   \
+        }                                                                                    \
     } while (0)
+
+#define JSON_GET_INT(object, string) (cJSON_GetObjectItem(object, string) != NULL ? cJSON_GetObjectItem(object, string)->valueint : -1)
+#define JSON_GET_DOUBLE(object, string) (cJSON_GetObjectItem(object, string) != NULL ? cJSON_GetObjectItem(object, string)->valuedouble : -1)
+#define JSON_GET_STRING(object, string) (cJSON_GetObjectItem(object, string) != NULL ? cJSON_GetObjectItem(object, string)->valuestring : NULL)
 
 #ifdef __cplusplus
 }
