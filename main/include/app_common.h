92857e957b7f8639e7e96eda67a56c817e1cd611
César Magalhães <ceanma@gmail.com>
2020-08-23 18:58:29 -0300

    A função APP_ERROR_CHECK foi atualizada

--- main/include/app_common.h
+++ main/include/app_common.h
@@ -16,14 +16,13 @@ extern "C" {
 
 #define APP_COMMON_TAG "app_common"
 
-#define APP_ERROR_CHECK(a, str, goto_tag, ...)                                               \
-    do                                                                                       \
-    {                                                                                        \
-        if (!(a))                                                                            \
-        {                                                                                    \
-            ESP_LOGE(APP_COMMON_TAG, "%s(%d): " str, __FUNCTION__, __LINE__, ##__VA_ARGS__); \
-            goto goto_tag;                                                                   \
-        }                                                                                    \
+#define APP_ERROR_CHECK(valid, message, goto_tag, ...)                                                 \
+    do {                                                                                               \
+        if (!(valid)) {                                                                                \
+			if(*message != '\0')                                                                       \
+            	ESP_LOGE(APP_COMMON_TAG, "%s(%d): " message, __FUNCTION__, __LINE__, ##__VA_ARGS__);   \
+            goto goto_tag;                                                                             \
+        }                                                                                              \
     } while (0)
 
 #define JSON_GET_INT(object, string) (cJSON_GetObjectItem(object, string) != NULL ? cJSON_GetObjectItem(object, string)->valueint : -1)
